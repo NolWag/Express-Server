f24d54656f82b0a3ac9b44b89db9d3fb82d94b0c
NolWag <NolanDubyu@gmail.com>
2018-11-20 06:38:53 -0600

    initial

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('./scss/styles.scss')
+  .pipe(sass())
+    .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./scss/*.scss', ['sass'])
+  //gulp.watch('./js/*.js', ['js'])
+});
+
+gulp.task('default', ['sass', 'watch']);
